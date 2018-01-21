05334844596158e51db29b83615011a7979f4da2
Charlie Jackson <charlie.jackson@eventstag.com>
2018-01-21 20:10:43 +0000

    Get existing times

--- form.js
+++ form.js
@@ -1,15 +1,45 @@
 var t = TrelloPowerUp.iframe();
 
+var context = t.getContext();
+
+var baseUrl =
+  'https://script.google.com/a/charliejackson.com/macros/s/AKfycbx1wqNJ0XN0alA-5_E3E2gynKedBqeVSCmIa_Ai5DuJMmw28FI/exec?';
+
 t.render(function() {
   t.sizeTo('#timesheet').done();
   window.timesheetDate.value = new Date().toISOString().split('T')[0];
+
+  function error(e) {
+    console.error(e);
+  }
+
+  var url = baseUrl;
+  url += '&cardId=' + encodeURIComponent(context.card);
+  url += '&member=' + encodeURIComponent(context.member);
+
+  fetch(url, { redirect: 'follow' })
+    .then(function(res) {
+      console.warn('res', res);
+
+      if (res.ok) {
+        return res.json();
+      }
+
+      console.error(res);
+
+      error(new Error('Non 200 status, could not get: ' + res.status));
+    })
+    .then(function(data) {
+      console.warn('data', data);
+    })
+    .catch(function(e) {
+      error(e);
+    });
 });
 
 window.timesheet.addEventListener('submit', function(event) {
   // Stop the browser trying to submit the form itself.
   event.preventDefault();
-
-  var context = t.getContext();
 
   return t
     .set('card', 'shared', 'lastSetTimesheet', window.timesheetDate.value)
@@ -28,9 +58,7 @@ window.timesheet.addEventListener('submit', function(event) {
         console.error(e);
       }
 
-      var url =
-        'https://script.google.com/a/charliejackson.com/macros/s/AKfycbx1wqNJ0XN0alA-5_E3E2gynKedBqeVSCmIa_Ai5DuJMmw28FI/exec?';
-
+      var url = baseUrl;
       url += 'date=' + encodeURIComponent(window.timesheetDate.value);
       url += '&time=' + encodeURIComponent(window.timesheetTime.value);
       url += '&notes=' + encodeURIComponent(window.timesheetNotes.value);
@@ -60,8 +88,7 @@ window.timesheet.addEventListener('submit', function(event) {
           );
         })
         .then(function(data) {
-          console.warn('data', data.parameter);
-          t.closeModal();
+          console.warn('data', data);
         })
         .catch(function(e) {
           error(e);
