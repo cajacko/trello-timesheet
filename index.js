8ae8e54771adbb8af75a5f1f6eef6928f4192908
Charlie Jackson <charlie.jackson@eventstag.com>
2018-01-21 15:06:22 +0000

    Added a basic form

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-console.warn('LOADED BABY');
-
 TrelloPowerUp.initialize({
   'card-buttons': function(t, options) {
     return [
       {
         icon:
           'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
-        text: 'Estimate Size',
+        text: 'New Time Entry',
+        callback: function(t) {
+          return t.popup({
+            title: 'New Time Entry',
+            url: 'https://cajacko.github.io/trello-timesheet/form.html',
+          });
+        },
       },
     ];
   },
