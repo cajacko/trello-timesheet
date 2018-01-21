d9df59b4af75d4b3cd261a82dea0f557c1cf76e7
Charlie Jackson <charlie.jackson@eventstag.com>
2018-01-21 15:19:39 +0000

    Fixed js error

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-var GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg';
+var GRAY_ICON =
+  'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg';
 
 TrelloPowerUp.initialize({
-  'attachment-sections': function(t, options){
+  'attachment-sections': function(t, options) {
     // options.entries is a list of the attachments for this card
     // you can look through them and 'claim' any that you want to
     // include in your section.
 
     // we will just claim urls for Yellowstone
-    var claimed = options.entries.filter(function (attachment) {
+    var claimed = options.entries.filter(function(attachment) {
       return attachment.url.indexOf('http://www.nps.gov/yell/') === 0;
     });
 
@@ -19,23 +20,28 @@ TrelloPowerUp.initialize({
       // potentially lengthy operation you can provide a function for the title
       // that returns the section title. If you do so, provide a unique id for
       // your section
-      return [{
-        id: 'Yellowstone', // optional if you aren't using a function for the title
-        claimed: claimed,
-        icon: GRAY_ICON, // Must be a gray icon, colored icons not allowed.
-        title: 'Example Attachment Section: Yellowstone',
-        content: {
-          type: 'iframe',
-          url: t.signUrl('https://cajacko.github.io/trello-timesheet/section.html', {
-            arg: 'you can pass your section args here'
-          }),
-          height: 230
-        }
-      }];
+      return [
+        {
+          id: 'Yellowstone', // optional if you aren't using a function for the title
+          claimed: claimed,
+          icon: GRAY_ICON, // Must be a gray icon, colored icons not allowed.
+          title: 'Example Attachment Section: Yellowstone',
+          content: {
+            type: 'iframe',
+            url: t.signUrl(
+              'https://cajacko.github.io/trello-timesheet/section.html',
+              {
+                arg: 'you can pass your section args here',
+              },
+            ),
+            height: 230,
+          },
+        },
+      ];
     } else {
       return [];
     }
-  }
+  },
 
   'card-buttons': function(t, options) {
     return [
