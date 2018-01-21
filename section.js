f5145152dee49fbe95742583b2ca7ef9a91334f4
Charlie Jackson <charlie.jackson@eventstag.com>
2018-01-21 15:25:33 +0000

    Show section

--- section.js
+++ section.js
@@ -1,9 +1,13 @@
 var t = window.TrelloPowerUp.iframe();
 
+console.warn('Section html');
+
 // you can access arguments passed to your iframe like so
 var arg = t.arg('arg');
 
 t.render(function() {
+  console.warn('Attachment section render');
+
   // make sure your rendering logic lives here, since we will
   // recall this method as the user adds and removes attachments
   // from your section
