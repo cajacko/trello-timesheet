e5e530ad8a90b21624105cd9078a6bf8fa8f7713
Charlie Jackson <charlie.jackson@eventstag.com>
2018-01-21 19:32:03 +0000

    Modal rather than popover

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.warn('HERE I AM BABY', 24);
+console.warn('HERE I AM BABY', 25);
 
 var GRAY_ICON =
   'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg';
@@ -11,7 +11,7 @@ TrelloPowerUp.initialize({
           'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
         text: 'New Time Entry',
         callback: function(t) {
-          return t.popup({
+          return t.modal({
             title: 'New Time Entry',
             url: 'https://cajacko.github.io/trello-timesheet/form.html',
           });
