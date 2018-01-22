c67338b93abe464ec412a8d7d23e2d2168a5d3d3
Charlie Jackson <charlie.jackson@eventstag.com>
2018-01-22 09:26:47 +0000

    Insert time entry if submitted successfully

--- form.js
+++ form.js
@@ -1,3 +1,4 @@
+var total = 0;
 var t = TrelloPowerUp.iframe();
 
 var context = t.getContext();
@@ -5,21 +6,38 @@ var context = t.getContext();
 var baseUrl =
   'https://script.google.com/a/charliejackson.com/macros/s/AKfycbx1wqNJ0XN0alA-5_E3E2gynKedBqeVSCmIa_Ai5DuJMmw28FI/exec?';
 
+function addRow(row) {
+  var date = new Date(row[0]).toISOString().split('T')[0];
+
+  var html =
+    '<tr><td>' +
+    date +
+    '</td><td>' +
+    row[2] +
+    '</td><td>' +
+    row[3] +
+    '</td></tr>';
+
+  document.getElementById('tbody').insertAdjacentHTML('afterbegin', html);
+  total += row[2];
+}
+
 function getTime() {
   function error(e) {
     console.error(e);
   }
 
+  context = {
+    card: '5a5f3a8589cc03e516be856a',
+    member: '50ed911f70cb352222002f03',
+  };
+
   var url = baseUrl;
   url += '&cardId=' + encodeURIComponent(context.card);
   url += '&member=' + encodeURIComponent(context.member);
 
-  console.warn(url);
-
   fetch(url, { redirect: 'follow' })
     .then(function(res) {
-      console.warn('res', res);
-
       if (res.ok) {
         return res.json();
       }
@@ -29,27 +47,13 @@ function getTime() {
       error(new Error('Non 200 status, could not get: ' + res.status));
     })
     .then(function(data) {
-      console.warn('data', data);
-      var html;
-      var row;
-      var element = document.getElementById('tbody');
-      var total = 0;
+      total = 0;
 
       for (var i = 0; i < data.length; i++) {
-        row = data[i];
-        html =
-          '<tr><td>' +
-          row[0] +
-          '</td><td>' +
-          row[2] +
-          '</td><td>' +
-          row[3] +
-          '</td></tr>';
-        element.insertAdjacentHTML('beforeend', html);
-        total += row[2];
+        addRow(data[i]);
       }
 
-      element.style.display = 'block';
+      document.getElementById('history').style.display = 'block';
       document.getElementById('loading').style.display = 'none';
       document.getElementById('total').textContent = total;
     })
@@ -57,6 +61,8 @@ function getTime() {
       error(e);
     });
 }
+
+getTime();
 
 t.render(function() {
   t.sizeTo('#timesheet').done();
@@ -80,8 +86,6 @@ window.timesheet.addEventListener('submit', function(event) {
         member: context.member,
       };
 
-      console.warn(payload);
-
       function error(e) {
         console.error(e);
       }
@@ -93,18 +97,8 @@ window.timesheet.addEventListener('submit', function(event) {
       url += '&cardId=' + encodeURIComponent(context.card);
       url += '&member=' + encodeURIComponent(context.member);
 
-      fetch(url, {
-        // headers: {
-        //   Accept: 'application/json',
-        //   'Content-Type': 'application/json',
-        // },
-        method: 'GET',
-        redirect: 'follow',
-        // body: JSON.stringify(payload),
-      })
+      fetch(url, { redirect: 'follow' })
         .then(function(res) {
-          console.warn('res', res);
-
           if (res.ok) {
             return res.json();
           }
@@ -116,7 +110,7 @@ window.timesheet.addEventListener('submit', function(event) {
           );
         })
         .then(function(data) {
-          console.warn('data', data);
+          addRow([payload.date, payload.cardId, payload.time, payload.notes]);
         })
         .catch(function(e) {
           error(e);
--- index.js
+++ index.js
@@ -9,12 +9,12 @@ TrelloPowerUp.initialize({
       {
         icon:
           'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
-        text: 'New Time Entry',
+        text: 'Timesheet',
         callback: function(t) {
           return t.modal({
-            title: 'New Time Entry',
+            title: 'Timesheet',
             url: 'https://cajacko.github.io/trello-timesheet/form.html',
-            height: 500,
+            height: 800,
           });
         },
       },
