dd7c4aa5e627ac80f7b580815e4aadeb8969f225
Charlie Jackson <charlie.jackson@eventstag.com>
2018-01-22 07:58:41 +0000

    Show existing times

--- form.js
+++ form.js
@@ -5,17 +5,21 @@ var context = t.getContext();
 var baseUrl =
   'https://script.google.com/a/charliejackson.com/macros/s/AKfycbx1wqNJ0XN0alA-5_E3E2gynKedBqeVSCmIa_Ai5DuJMmw28FI/exec?';
 
-t.render(function() {
-  t.sizeTo('#timesheet').done();
-  window.timesheetDate.value = new Date().toISOString().split('T')[0];
-
+function getTime() {
   function error(e) {
     console.error(e);
   }
 
+  context = {
+    card: '5a5f3a8589cc03e516be856a',
+    member: '50ed911f70cb352222002f03',
+  };
+
   var url = baseUrl;
   url += '&cardId=' + encodeURIComponent(context.card);
   url += '&member=' + encodeURIComponent(context.member);
+
+  console.warn(url);
 
   fetch(url, { redirect: 'follow' })
     .then(function(res) {
@@ -31,10 +35,41 @@ t.render(function() {
     })
     .then(function(data) {
       console.warn('data', data);
+      var html;
+      var row;
+      var element = document.getElementById('tbody');
+      var total = 0;
+
+      for (var i = 0; i < data.length; i++) {
+        row = data[i];
+        html =
+          '<tr><td>' +
+          row[0] +
+          '</td><td>' +
+          row[2] +
+          '</td><td>' +
+          row[3] +
+          '</td></tr>';
+        element.insertAdjacentHTML('beforeend', html);
+        total += row[2];
+      }
+
+      element.style.display = 'block';
+      document.getElementById('loading').style.display = 'none';
+      document.getElementById('total').textContent = total;
     })
     .catch(function(e) {
       error(e);
     });
+}
+
+// getTime();
+
+t.render(function() {
+  t.sizeTo('#timesheet').done();
+  window.timesheetDate.value = new Date().toISOString().split('T')[0];
+
+  getTime();
 });
 
 window.timesheet.addEventListener('submit', function(event) {
